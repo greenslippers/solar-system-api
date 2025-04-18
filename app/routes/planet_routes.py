fe808607cbc0ffa2097cb22c743989f5ec8c3190
Malik Elmessiry <malik.messiry@gmail.com>
2025-04-18 20:30:07 +0200

    completed get_one_planet function with helper function

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from ..models.planet import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix = "/planets")
@@ -14,3 +14,27 @@ def get_all_planets():
             color = planet.color
         ))
     return result_list
+
+@planets_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    for planet in planets:
+        return dict(
+            id = planet.id,
+            name = planet.name,
+            description = planet.description,
+            color = planet.color
+        )
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        response = {"message": f"planet {planet_id} invalid"}
+        abort(make_response(response, 400))
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    response = {"message": f"planet {planet_id} not found"}
+    abort(make_response(response, 404))
+        
