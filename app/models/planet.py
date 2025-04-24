5969cefcddbe4d9d3ef935d7f7660cbbf12cdd4a
Malik Elmessiry <malik.messiry@gmail.com>
2025-04-24 20:34:30 +0200

    wave 3 planet model setup

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,13 +1,32 @@
-class Planet:
-    def __init__(self, id, name, description, color):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.color = color
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
 
-planets = [
-    Planet(1, "Earth", "Home planet", "blue"),
-    Planet(2, "Venus", "Second planet", "red"),
-    Planet(3, "Saturn", "Has rings", "purple")
-]
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    color: Mapped[str]
+
+
+
+
+
+
+
+
+
+
+
+# class Planet:
+#     def __init__(self, id, name, description, color):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.color = color
+
+# planets = [
+#     Planet(1, "Earth", "Home planet", "blue"),
+#     Planet(2, "Venus", "Second planet", "red"),
+#     Planet(3, "Saturn", "Has rings", "purple")
+# ]
     
--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,39 +1,48 @@
 from flask import Blueprint, abort, make_response
-from ..models.planet import planets
+# from ..models.planet import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix = "/planets")
 
-@planets_bp.get("")
-def get_all_planets():
-    result_list = []
-    for planet in planets:
-        result_list.append(dict(
-            id = planet.id,
-            name = planet.name,
-            description = planet.description,
-            color = planet.color
-        ))
-    return result_list
 
-@planets_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
-    return dict(
-        id = planet.id,
-        name = planet.name,
-        description = planet.description,
-        color = planet.color
-    )
 
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        response = {"message": f"planet {planet_id} invalid"}
-        abort(make_response(response, 400))
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
-    response = {"message": f"planet {planet_id} not found"}
-    abort(make_response(response, 404))
+
+
+
+
+
+
+
+# @planets_bp.get("")
+# def get_all_planets():
+#     result_list = []
+#     for planet in planets:
+#         result_list.append(dict(
+#             id = planet.id,
+#             name = planet.name,
+#             description = planet.description,
+#             color = planet.color
+#         ))
+#     return result_list
+
+# @planets_bp.get("/<planet_id>")
+# def get_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
+#     return dict(
+#         id = planet.id,
+#         name = planet.name,
+#         description = planet.description,
+#         color = planet.color
+#     )
+
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         response = {"message": f"planet {planet_id} invalid"}
+#         abort(make_response(response, 400))
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
+#     response = {"message": f"planet {planet_id} not found"}
+#     abort(make_response(response, 404))
         
