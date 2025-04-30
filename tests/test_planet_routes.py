62c49a7d79149f9ccde8c4e7d8787693adbfc722
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-04-30 14:22:16 -0700

    Wave 6 completed

---
+++ tests/test_planet_routes.py
@@ -0,0 +1,40 @@
+def test_get_all_planets_with_no_records(client):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_one_planet(client, two_saved_planets):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "Mercury",
+        "description": "the smallest planet",
+        "color": "gray"
+    }
+
+def test_create_one_planet(client):
+    # Act
+    response = client.post("/planets", json={
+        "name": "Venus",
+        "description": "beautiful",
+        "color": "red"
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body == {
+        "id": 1,
+        "name": "Venus",
+        "description": "beautiful",
+        "color": "red"
+    }
